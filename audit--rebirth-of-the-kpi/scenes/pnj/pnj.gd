0673af342b0974c5e15afed6c10ccb340cedb6ec
Elisabeth <elisabeth.maupas@etu.unistra.fr>
2025-09-26 14:08:12 +0200

    A player, a pnj and a map with collisions

--- audit--rebirth-of-the-kpi/scenes/pnj/pnj.gd
+++ audit--rebirth-of-the-kpi/scenes/pnj/pnj.gd
@@ -1,15 +1,22 @@
-extends Area2D
+extends  StaticBody2D
 
 signal dialogue_requested #Signal pour le dialogue 
-
-func _ready() -> void:
-	pass # Replace with function body.
+var player_in_range = false
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
+	if player_in_range and Input.is_action_just_pressed("ui_accept"):
 		emit_signal("dialogue_requested")
-	
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
-	if (event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT):
+	if (player_in_range and (event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT)):
 		emit_signal("dialogue_requested")
+
+func _on_talknode_body_entered(body: Node2D) -> void:
+	print("Hello")
+	if body.name == "player":
+		player_in_range = true
+
+
+func _on_talknode_body_exited(body: Node2D) -> void:
+	if body.name == "player":
+		player_in_range = false

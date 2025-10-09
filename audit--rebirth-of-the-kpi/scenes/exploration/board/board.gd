53a9d53018da4138ef89afb11f9d296fb92db77e
Elisabeth <elisabeth.maupas@etu.unistra.fr>
2025-10-09 12:05:50 +0200

    Ajout d'un uml, de classes et j'ai un peu rangé

---
+++ audit--rebirth-of-the-kpi/scenes/exploration/board/board.gd
@@ -0,0 +1,11 @@
+class_name Board
+extends TileMapLayer
+
+func _ready() -> void:	
+	$pnj.connect("dialogue_requested", Callable(self,"on_dialogue_requested"))
+
+func on_dialogue_requested() -> void:
+	if ($dialogueBox.get_panel_visible()==true):
+		$dialogueBox.next_line()
+	else:
+		$dialogueBox.start_dialogue()

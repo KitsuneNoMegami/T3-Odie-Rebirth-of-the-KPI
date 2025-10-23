6c45d42bba1de4e3b053a7a5413226667016e562
Elisabeth MAUPAS <elisabeth.maupas@etu.unistra.fr>
2025-10-23 13:38:15 +0000

    Revert "Merge branch 'menu' into 'main'"
    This reverts merge request !12

--- audit--rebirth-of-the-kpi/scenes/menu/menu.gd
+++ audit--rebirth-of-the-kpi/scenes/menu/menu.gd
@@ -5,37 +5,37 @@ extends Node
 @onready var exit: Label = $exit
 
 var text: Array[Label]
-var nb := 4
+var nb := 0
 
 func _ready() -> void:
 	text = [start, settings, exit]
 	# Connexion du survol de la souris
 	for i in range(text.size()):
 		text[i].connect("mouse_entered", Callable(self, "_on_label_hovered").bind(i))
-		text[i].connect("mouse_exited",Callable(self,"_on_label_hovered").bind(4))
-	update_selection()
+
+	_update_selection()
 
 func _input(event: InputEvent) -> void:
 	# Flèche bas
 	if event.is_action_pressed("down"):
 		nb = (nb + 1) % text.size()
-		update_selection()
+		_update_selection()
 
 	# Flèche haut
 	elif event.is_action_pressed("up"):
 		nb = (nb - 1 + text.size()) % text.size()
-		update_selection()
+		_update_selection()
 
 	# Entrée ou clic gauche (car accept contient les deux)
 	elif event.is_action_pressed("accept"):
-		trigger_action(nb)
+		_trigger_action(nb)
 
 func _on_label_hovered(i: int) -> void:
 	nb = i
-	update_selection()
+	_update_selection()
 
 #reagis à la l'input accept 
-func trigger_action(i: int) -> void:
+func _trigger_action(i: int) -> void:
 	match i:
 		0:
 			get_tree().change_scene_to_file("res://scenes/exploration/board/board.tscn")
@@ -46,7 +46,7 @@ func trigger_action(i: int) -> void:
 			get_tree().quit()
 
 #met a jour la taille de chaque label quand nécéssaire
-func update_selection() -> void:
+func _update_selection() -> void:
 	for i in range(text.size()):
 		if i == nb:
 			text[i].add_theme_font_size_override("font_size", 125)

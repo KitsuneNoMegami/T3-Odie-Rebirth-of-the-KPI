cd4863148055f79ff7d9551ca05e893322e6c1d5
Beth <e_mops@hotmail.com>
2025-12-02 14:55:05 +0100

    Building over OK sauf commercial

--- audit--rebirth-of-the-kpi/scenes/exploration/board/pause/pause_menu.gd
+++ audit--rebirth-of-the-kpi/scenes/exploration/board/pause/pause_menu.gd
@@ -38,9 +38,16 @@ func pause_unpause():
 		get_tree().paused=false
 
 func _input(event: InputEvent) -> void:
+	if not visible :
+		return
 	if event.is_action_pressed("pause"):
 		print('aaaaa')
 		pause_unpause()
+		await get_tree().create_timer(0.1).timeout
+		hide()
+		get_tree().paused=false
+		pause=!pause
+
 	
 	# Flèche bas
 	if event.is_action_pressed("down"):

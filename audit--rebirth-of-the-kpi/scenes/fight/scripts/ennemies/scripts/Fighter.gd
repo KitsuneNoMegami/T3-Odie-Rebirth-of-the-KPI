f07a1a6b65145bfdce1261ec6cb70997ec1f2b37
Joseph Cuiller <jcuiller@unistra.fr>
2025-12-13 15:04:13 +0100

    move ennemies to fight/script

--- audit--rebirth-of-the-kpi/scenes/fight/scripts/ennemies/scripts/Fighter.gd
+++ audit--rebirth-of-the-kpi/scenes/fight/scripts/ennemies/scripts/Fighter.gd
@@ -1,4 +1,4 @@
-class_name Fighter extends AnimatedSprite2D
+class_name Fighter extends Node2D
 
 var _name
 var _pv
@@ -9,7 +9,6 @@ var _levelSkill=0
 var _attacks
 var _defenses
 var _pts_defense =0
-var _sprite=null
 
 func _ready():
 	pass
@@ -19,7 +18,6 @@ func _init(pv=null,fname=null,attacks=null,defenses=null):
 	_pv_max=pv
 	_description="Ceci est la description du personnage du pole"
 	_name=fname
-	_sprite=fname
 	_attacks=attacks
 	_defenses=defenses
 	
@@ -56,5 +54,5 @@ func get_description():
 func get_pv():
 	return _pv
 
-func _play():
-	$sprite.play(_sprite)
+func set_sprite(sprite):
+	$sprite.play(sprite)

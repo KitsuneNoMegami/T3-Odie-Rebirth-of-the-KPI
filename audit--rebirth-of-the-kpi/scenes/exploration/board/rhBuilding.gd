b154fa2ef68194e9f82b6c5359fb3a71f97315a2
Kilian <kilian.sakhi@etu.unistra.fr>
2025-12-14 23:42:47 +0100

    Tooltip d'un batîment lorsque qu'il est survolé par la souris

--- audit--rebirth-of-the-kpi/scenes/exploration/board/rhBuilding.gd
+++ audit--rebirth-of-the-kpi/scenes/exploration/board/rhBuilding.gd
@@ -14,3 +14,29 @@ func _process(delta: float) -> void:
 func _on_pressed() -> void:
 	const office_scene=preload("res://scenes/exploration/board/offices/rhOffice.tscn")
 	get_tree().change_scene_to_packed(office_scene)
+
+
+func _on_mouse_entered() -> void:
+	var txt_box = get_parent().get_node("txt_box_rh")
+	txt_box.show()
+	pass # Replace with function body.
+
+
+
+
+func _on_mouse_exited() -> void:
+	var txt_box = get_parent().get_node("txt_box_rh")
+	txt_box.hide()
+	pass # Replace with function body.
+
+
+func _on_finances_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_finances_mouse_exited() -> void:
+	pass # Replace with function body.
+
+
+func _on_it_mouse_entered() -> void:
+	pass # Replace with function body.

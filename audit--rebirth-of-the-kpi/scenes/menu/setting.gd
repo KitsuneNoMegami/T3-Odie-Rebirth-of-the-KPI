7c947020e316ecf8bf87d6d6dd24b5282d53d046
Beth <e_mops@hotmail.com>
2025-12-17 16:18:38 +0100

    ajout sprites prof + menu start + design boutons

--- audit--rebirth-of-the-kpi/scenes/menu/setting.gd
+++ audit--rebirth-of-the-kpi/scenes/menu/setting.gd
@@ -12,4 +12,4 @@ func _process(_delta: float) -> void:
 
 
 func _on_pressed() -> void:
-	get_tree().quit()
+	pass # Replace with function body.
--- audit--rebirth-of-the-kpi/scenes/menu/start.gd
+++ audit--rebirth-of-the-kpi/scenes/menu/start.gd
@@ -10,6 +10,5 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
-
 func _on_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://scenes/exploration/board/mapMario.tscn")

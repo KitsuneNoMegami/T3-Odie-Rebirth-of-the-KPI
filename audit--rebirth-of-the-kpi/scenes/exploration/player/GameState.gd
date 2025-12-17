5fc2eecec4eb848f640b9e337ef6e97e045b50a5
Kilian <kilian.sakhi@etu.unistra.fr>
2025-12-17 15:47:42 +0100

    BugFix: V0.8

--- audit--rebirth-of-the-kpi/scenes/exploration/player/GameState.gd
+++ audit--rebirth-of-the-kpi/scenes/exploration/player/GameState.gd
@@ -59,6 +59,7 @@ func get_player():
 ## pole:String - Nom du pôle à combattre
 func start_fight(path, pole):
 	_pole=pole
+	print(pole)
 	if get_win(pole)==null:
 		fight_scene.fight_unfight(path,pole,_player)
 

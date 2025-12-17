9108d2d3de5cbedfda9496eef8725d240fccc4da
jonathan <jbevilacqua@unistra.fr>
2025-12-17 20:18:27 +0100

    précision dans le tutoriel

--- audit--rebirth-of-the-kpi/scenes/exploration/board/Tuto.gd
+++ audit--rebirth-of-the-kpi/scenes/exploration/board/Tuto.gd
@@ -5,6 +5,9 @@ extends TileMapLayer
 var txt1 = "Bonjour, c'est moi [Nom], le directeur général de la Z corp, c'est moi qui est fait appel a vous afin de réaliser l'audit interne de l'entreprise."
 var txt2 = "Pour ce faire, vous allez devoir vous rendre dans les différents pôles de l'entreprise (en cliquant dessus)."
 var txt3 = "Puis vous devrez inspecter les pôles en interrogeant le chef de département correspondant. Attention cependant il pourrait vous causez un peu de tort."
+var txt4 = "Pour vous aidez pendant votre audit veuillez utilisez les flèches directionnelles du clavier pour vous déplacez et appuyez sur entrée ou le clique de la souris pour démarrer un dialogue ou validez une action.
+Durant les combats ; si part erreur vous réalisez une action involontaire il vous sera possible de retourner sur le menu précedent à tout moment avec la touche delete"
+var txt5= "Sur ce je vous souhaite bonne chance je retourne vaquer à mes occupations"
 var speech_part= 0
 @export var tuto_txt :RichTextLabel
 

9a2ee0c22d077ea03d18fbe548ebc9e2297cf050
Kilian <kilian.sakhi@etu.unistra.fr>
2025-12-18 12:53:01 +0100

    merge fix

--- audit--rebirth-of-the-kpi/scenes/exploration/board/todo.gd
+++ audit--rebirth-of-the-kpi/scenes/exploration/board/todo.gd
@@ -3,7 +3,7 @@ extends RichTextLabel
 func _process(_delta: float) -> void:
 	clear()
 	var poles=["communication","it","commercial","finance","rh"]
-	append_text("TODO List [b]:[/b]\n")
+	append_text("TODO List [b]:[/b]\n\n\n")
 	for p in poles:
 		if GameState.get_win(p)!=null:
 			append_text("[s][b]--[/b] "+p+"[/s]\n")
